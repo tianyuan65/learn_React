597033a791b4b889566da228612abe2e6eed1600
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2023-02-11 18:01:39 +0900

    路由

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+// 引入Header
+import Header from './components/Header'
+// 引入List
+import List from './components/List'
+// 引入Footer
+import Footer from './components/Footer'
+// 引入样式
+import './App.css'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h2 style={{fontSize:'40px'}}>App....</h2>
+      <div className="todo-container">
+        <div className="todo-wrap">
+          <Header/>
+          <List/>
+          <Footer/>
+        </div>
       </div>
     )
   }
