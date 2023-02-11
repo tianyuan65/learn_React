cf6de7bd6437dc3b13edb7a7e073b612f341fcb8
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2023-02-11 18:33:53 +0900

    static component

--- react_staging/src/components/Item/Item.jsx
+++ react_staging/src/components/Item/Item.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import './Item.css'
 
 export default class Item extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <li>
+        <label>
+          <input type="checkbox"/>
+          <span>XXXXX</span>
+        </label>
+        <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+      </li>
     )
   }
 }
