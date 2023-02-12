987d2974866f21bdb25d960683268c171297d689
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2023-02-12 20:36:57 +0900

    dynamic initialization list

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react'
 // 引入Header
-import Header from './components/Header'
+import Header from './components/Header/Header'
 // 引入List
-import List from './components/List'
+import List from './components/List/List'
 // 引入Footer
-import Footer from './components/Footer'
+import Footer from './components/Footer/Footer'
 // 引入样式
 import './App.css'
 
 export default class App extends Component {
+
+  // 初始化状态
+  state={todos:[
+    {id:'001',name:'吃饭',done:true},
+    {id:'002',name:'睡觉',done:false},
+    {id:'003',name:'打游戏',done:true},
+    {id:'004',name:'做白日梦',done:false}
+
+  ]}
+
   render() {
+    const {todos}=this.state
     return (
       <div className="todo-container">
         <div className="todo-wrap">
           <Header/>
-          <List/>
+          <List todos={todos}/>
           <Footer/>
         </div>
       </div>
--- react_staging/src/components/List/List.jsx
+++ react_staging/src/components/List/List.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
-import Item from '../Item'
+import Item from '../Item/Item'
 import './List.css'
 
 export default class List extends Component {
   render() {
+    const {todos}=this.props
     return (
       <ul className="todo-main">
-        <Item/>
+        {
+          todos.map(todo=>{
+            return <Item key={todo.id} {...todo}/>
+          })
+        }
+
       </ul>
     )
   }
