ed7a82e79e71566c83211a9d2444761cd1951cba
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2023-02-15 14:31:44 +0900

    mouse moved

--- react_staging/src/components/Item/Item.jsx
+++ react_staging/src/components/Item/Item.jsx
@@ -2,15 +2,23 @@ import React, { Component } from 'react'
 import './Item.css'
 
 export default class Item extends Component {
+
+  state={mouse:false}
+  handleMouse=(flag)=>{
+    return ()=>{
+      this.setState({mouse:flag});
+    }
+  }
   render() {
     const {name,done}=this.props
+    const {mouse}=this.state
     return (
-      <li>
+      <li style={{backgroundColor:mouse ? '#ddd' : 'white'}} onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)}>
         <label>
           <input type="checkbox" defaultChecked={done}/>
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+        <button className="btn btn-danger" style={{display:mouse ? 'block' : 'none'}}>删除</button>
       </li>
     )
   }
