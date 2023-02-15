51667cf8d4c3deda369ba8ce7c54d7cae7a47bcb
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2023-02-15 17:40:04 +0900

    add a todo

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -9,13 +9,14 @@ import Footer from './components/Footer/Footer'
 import './App.css'
 
 export default class App extends Component {
+  // 状态在哪里，操作状态的方法就在哪里
 
   // 初始化状态
   state={todos:[
     {id:'001',name:'吃饭',done:true},
     {id:'002',name:'睡觉',done:false},
     {id:'003',name:'打游戏',done:true},
-    {id:'004',name:'做白日梦',done:false}
+    {id:'004',name:'做白日梦',done:true}
 
   ]}
 
@@ -29,13 +30,27 @@ export default class App extends Component {
     this.setState({todos:newTodos})
   }
 
+  // updateTodo用于更新一个todo对象
+  updateTodo=(id,done)=>{
+    // 获取状态中的todos
+    const {todos}=this.state
+    // 匹配处理数据
+    const newTodos=todos.map((todoObj)=>{
+      // 如果新添加的id和原来的id相等的话，就返回新的todo对象和更改后的done的值
+      if(todoObj.id===id) return {...todoObj,done}
+      // 如果匹配结果不同，原来的是什么就返回什么
+      else return todoObj
+    })
+    this.setState({todos:newTodos})
+  }
+
   render() {
     const {todos}=this.state
     return (
       <div className="todo-container">
         <div className="todo-wrap">
           <Header addTodo={this.addTodo}/>
-          <List todos={todos}/>
+          <List todos={todos} updateTodo={this.updateTodo}/>
           <Footer/>
         </div>
       </div>
--- react_staging/src/components/Item/Item.jsx
+++ react_staging/src/components/Item/Item.jsx
@@ -3,19 +3,29 @@ import './Item.css'
 
 export default class Item extends Component {
 
+  // 标识鼠标移入、移出
   state={mouse:false}
+  // 鼠标移入、移出的回调
   handleMouse=(flag)=>{
     return ()=>{
       this.setState({mouse:flag});
     }
   }
+
+  // 勾选、取消勾选某一个todo的回调
+  handleCheck=(id)=>{
+    return (event)=>{
+      this.props.updateTodo(id,event.target.checked)
+    }
+  }
+
   render() {
-    const {name,done}=this.props
+    const {id,name,done}=this.props
     const {mouse}=this.state
     return (
       <li style={{backgroundColor:mouse ? '#ddd' : 'white'}} onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)}>
         <label>
-          <input type="checkbox" defaultChecked={done}/>
+          <input type="checkbox" defaultChecked={done} onChange={this.handleCheck(id)}/>
           <span>{name}</span>
         </label>
         <button className="btn btn-danger" style={{display:mouse ? 'block' : 'none'}}>删除</button>
