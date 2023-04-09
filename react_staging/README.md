e49590415b9036825fa83e12c5d167bbcffe9927
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2023-04-09 16:28:55 +0900

    use router component basically

--- react_staging/README.md
+++ react_staging/README.md
@@ -10,3 +10,45 @@
     * --defaultChecked只在初次渲染的时候生效，更新时不受控制；checked在全过程中始终受到状态控制，必须通过绑定onChange事件来控制选中情况
 * 5. 状态在哪里，操作状态的方法就在哪里
     * --对todos增删改查的方法都是在父组件中完成的
+
+## 二、github搜索案例相关知识点
+* 1. 设计状态时要考虑全面，例如带有网络请求的组件，要考虑请求失败怎么办
+* 2. ES6小知识点：解构赋值+重命名
+    * ```
+        //传统解构赋值
+        let obj={a:{b:{c:1}}}
+        let obj2={a:{b:1}}
+        console.log(obj.a.b.c)  //1
+
+        //连续解构赋值
+        const {a:{b:{c}}}=obj
+        console.log(c)  //1
+
+        //连续解构赋值并重命名
+        const {a:{b:data}}=obj2
+        console.log(data)  //1
+      ```
+* 3. 消息订阅与发布机制
+    * 1. 限定月，再发布(理解：一种兄弟间隔空对话的感觉，父组件此时只是容纳子组件的容器)
+    * 2. 适用于任意组件间通信
+    * 3. 要在组件的componentWillUnmount中取消订阅
+* 4. fetch发送请求(关注分离的设计思想)
+    * ```
+        try {
+            const response=await fetch(`/api1/search/users2?q=${keyWord}`)
+            const data=await response.json()
+            PubSub.publish({isFirst:false,users:data.items})
+            console.log(data);
+        } catch (error) {
+            console.log('请求出错',error);
+            PubSub.publish({isFirst:false,err:error.message})
+        }
+      ```
+
+## 三、路由的基本使用
+* 1. 明确好界面中的导航区、展示区
+* 2. 导航区的a标签改为Link标签
+    * <Link to="/xxxxx">Demo</Link>
+* 3. 展示区写Route标签进行路径的匹配
+    * <Route path='/xxxx' component={Demo}>
+* 4. <App>的最外侧包裹一个<BrowserRouter>或<HashRouter>
--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from 'react'
-import Search from './components/Search'
-import List from './components/List'
+import {Link,BrowserRouter,Route} from 'react-router-dom'
+import Home from './pages/Home'  //Home是路由组件
+import About from './pages/About'  //About是路由组件
+import Header from './components/Header'  //Header是一般组件
 
 export default class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Search/>
-        <List/>
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <Header a={1}/>
+          </div>
+        </div>
+        <BrowserRouter>
+          <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/* 原生html中，靠<a>跳转到不同的页面 */}
+              {/* <a className="list-group-item active" href="./about.html">About</a>
+              <a className="list-group-item" href="./home.html">Home</a> */}
+
+              {/* 在React中靠路由链接实现切换组件(注意跳转地址写法：小写带杠，不加点)--编写路由链接 */}
+              <Link className="list-group-item" to="/about">About</Link>
+              <Link className="list-group-item" to="/home">Home</Link>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                {/* 注册路由，靠路由进行匹配，匹配成功才展示对应的组件内容 */}
+                <Route path="/about" component={About}/>
+                <Route path="/home" component={Home}/>
+              </div>
+            </div>
+          </div>
+        </div>
+        </BrowserRouter>
       </div>
     )
   }
