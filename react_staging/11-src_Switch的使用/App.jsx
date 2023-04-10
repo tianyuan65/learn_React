4d87e352db6b1647c621b8d67a12c0745af50762
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2023-04-10 15:40:55 +0900

    how use Switch

---
+++ react_staging/11-src_Switch的使用/App.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
+import Home from './pages/Home'  //Home是路由组件
+import About from './pages/About'  //About是路由组件
+import Header from './components/Header'  //Header是一般组件
+import MyNavLink from './components/MyNavLink'
+import Test from './pages/Test'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <Header a={1}/>
+          </div>
+        </div>
+        <BrowserRouter>
+          <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/* 原生html中，靠<a>跳转到不同的页面 */}
+              {/* <a className="list-group-item active" href="./about.html">About</a>
+              <a className="list-group-item" href="./home.html">Home</a> */}
+
+              {/* 在React中靠路由链接实现切换组件(注意跳转地址写法：小写带杠，不加点)--编写路由链接 */}
+              {/* 把标签名(about/home)作为标签体内容(About/Home)写在标签(<MyNavLink></MyNavLink>)里 */}
+              <MyNavLink to="/about" a={1} b={9} c={'11111'}>About</MyNavLink>
+              <MyNavLink to="/home">Home</MyNavLink>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                {/* 注册路由，靠路由进行匹配，匹配成功才展示对应的组件内容 */}
+                <Switch>
+                  <Route path="/about" component={About}/>
+                  <Route path="/home" component={Test}/>
+                  <Route path="/home" component={Home}/>
+                </Switch>
+               
+              </div>
+            </div>
+          </div>
+        </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
+}
