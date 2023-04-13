47751fb09bbb204c62938f149ef74e4c6aac9961
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2023-04-13 18:27:39 +0900

    basic use antd

--- react_staging/src/index.js
+++ react_staging/src/index.js
@@ -2,14 +2,7 @@
 import React from 'react'
 // 引入react-dom
 import ReactDOM from 'react-dom'
-// 引入react-router-dom中的HashRouter
-import {HashRouter} from 'react-router-dom'
 // 引入App
 import App from './App'
 
-ReactDOM.render(
-    <HashRouter>
-        <App/>
-    </HashRouter>,
-document.getElementById('root')
-)
+ReactDOM.render(<App/>,document.getElementById('root'))
